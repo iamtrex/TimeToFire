7d0ae1dc94c7b47389d75cf78a892a6fbfc6edc6
RWEQX <rexlin54@yahoo.ca>
2018-07-29 01:08:16 -0700

    Switching bases commit.

---
+++ src/components/AddIncomeForms/PassiveForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Title from "../Title";
+
+class ActiveForm extends Component {
+
+    constructor(){
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className={"PassiveForm"}>
+            </div>);
+    }
+}
+
+export default ActiveForm;
---
+++ src/components/AddIncomeForms/PortfolioForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Title from "../Title";
+
+class PortfolioForm extends Component {
+
+    constructor(){
+        super();
+    }
+
+    render() {
+
+        return (
+            <div className={"PortfolioForm"}>
+            </div>);
+    }
+}
+
+export default PortfolioForm;
